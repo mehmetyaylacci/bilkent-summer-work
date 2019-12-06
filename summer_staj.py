d8e10dc496d99bb25a4abe0b6d4b2e4c927321c4
Alp Sayin <alpsayin@gmail.com>
2019-12-07 00:07:57 +0300

    set download delay to 0.1s; no one needs this data that fast

--- summer_staj.py
+++ summer_staj.py
@@ -115,7 +115,7 @@ class second_spider(scrapy.Spider):
     def start_requests(self):
         # setting custom settings, this will hopefully solve a possible ddos.
         custom_settings = {
-            'DOWNLOAD_DELAY': 0.01,
+            'DOWNLOAD_DELAY': 0.1,
             'ITEM_PIPELINES': {
                 'freedom.pipelines.IndexPipeline': 300
             }
